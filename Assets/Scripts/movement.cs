09b25a784e934968731e4efdae767cd5ca3a7797
Wuggie <wuggie88@yahoo.co.uk>
2023-09-26 11:17:57 +0200

    initial movement
    Needs to be redone, it's shit

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public CharacterController controller;
+    public Vector3 playerVelocity;
+    public bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+    private void Start() {
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0) {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero) {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer) {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
